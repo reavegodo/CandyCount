83f4297dda9add83b2a836abd834d5e19cae50a3
reavegodo <56036113+reavegodo@users.noreply.github.com>
2019-10-03 23:16:10 +0200

    Create CandyCount.java

---
+++ CandyCount.java
@@ -0,0 +1,18 @@
+public class CandyCount {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+          double (money < 12.4); 
+           double (price < 1.2);
+           int (candies < 0);
+           if (money > 0 and price > 0);
+               while (money - price >= 0) {
+                   candies < candies + 1
+                   money < money - price);{
+    } while
+    } if
+           system.out.printIn("Candies"); {
+    }
+           {
+}
