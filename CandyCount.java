99bd6fee3201e6aa983a0d47395a213c5a0e9514
reavegodo <56036113+reavegodo@users.noreply.github.com>
2019-10-03 23:16:31 +0200

    Update CandyCount.java

--- CandyCount.java
+++ CandyCount.java
@@ -7,7 +7,7 @@ public class CandyCount {
            double (price < 1.2);
            int (candies < 0);
            if (money > 0 and price > 0);
-               while (money - price >= 0) {
+               while (money - price >= 0); {
                    candies < candies + 1
                    money < money - price);{
     } while
