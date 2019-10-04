678cd22b7da6f1d3b38dabd3215ab5b118c1dd92
reavegodo <56036113+reavegodo@users.noreply.github.com>
2019-10-04 09:56:25 +0200

    Update CandyCount.java

--- CandyCount.java
+++ CandyCount.java
@@ -1,18 +1,20 @@
-public class CandyCount {
+
+	public class CandyCount {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
 
-          double (money < 12.4); 
-           double (price < 1.2);
-           int (candies < 0);
-           if (money > 0 and price > 0)
-               while (money - price >= 0); {
-                   candies < candies + 1;
-                   money < money - price; {
-    } while {
-    } if {
-           system.out.printIn("Candies"); {
-    }
+          double money = 12.4; 
+           double price = 1.2;
+           int candies = 0;
+           if (money > 0 && price > 0)
            {
+               while (money - price >= 0)
+               {
+                   candies = candies + 1;
+                   money = money - price;
+               }
+           }
+    
+           System.out.println(candies); 
+    }
 }
